377876bb6d27c2ed90285be45103af13562ae55b
sikhauli <sikhaulivl@gmail.com>
2024-01-10 17:06:26 +0200

    fixing profile

--- api/main.js
+++ api/main.js
@@ -22,13 +22,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.disable('x-powered-by');
 
-// origin: 'https://job-finder-bhg5.onrender.com',
-// origin: 'http://localhost:5173',
-
 const corsOptions = {
-    origin: 'https://job-finder-bhg5.onrender.com',
+    origin: process.env.NODE_ENV === 'production' ? 'https://job-finder-bhg5.onrender.com' : 'http://localhost:5173',
     credentials: true,
 };
+
 
 //cors middleware
 app.use(cors(corsOptions));
